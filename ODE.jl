9fcc6157dff73fa79493329fb734c438230755a4
ben-va <benjamin.vandenbussche@agroparistech.fr>
2018-10-09 13:16:32 +0200

    eta_m

--- ODE.jl
+++ ODE.jl
@@ -19,14 +19,14 @@ function system(du, u, p, t)
     sigma_e   =p[10]      #
     tau_m     =p[11]      # adjustment speed of production
     tau_e     =p[12]      #
-    eta_c     =p[13]      # energy efficiency in consumption
+    eta_m     =p[13]      # energy efficiency in consumption
     r_b       =p[14]      # interest rate
     phi       =p[15]      # a_e / a_m
     gamma     =p[16]      # money illusion (inflation)
-    kappa_0    =p[17]      # output share
-    kappa_1    =p[18]      #
-    kappa_2    =p[19]      #
-    kappa_3    =p[20]      #
+    kappa_0   =p[17]      # output share
+    kappa_1   =p[18]      #
+    kappa_2   =p[19]      #
+    kappa_3   =p[20]      #
     uetoile   =p[21]      #
 
 
@@ -38,7 +38,7 @@ function system(du, u, p, t)
     f_tau_m = kappa_0+kappa_1*atan(kappa_2*pi_m+kappa_3)
 
     #
-    util = (gamma_e/gamma_e-1)*(eta_c+1/gamma_m)*((u[2]*sigma_m)/sigma_e)
+    util = (gamma_e/gamma_e-1)*(eta_m+1/gamma_m)*((u[2]*sigma_m)/sigma_e)
 
     #
     if(util <= uetoile)
@@ -49,16 +49,16 @@ function system(du, u, p, t)
     end
 
     #
-    f_u = (gamma_e/(gamma_e-1))*(eta_c*(1-f_tau_m))*(1-f_tau_e+1/gamma_m)*((u[2]*sigma_m)/sigma_e)
+    f_u = (gamma_e/(gamma_e-1))*(eta_m*(1-f_tau_m))*(1-f_tau_e+1/gamma_m)*((u[2]*sigma_m)/sigma_e)
 
     #
-    q_e = ((eta_c*(1-f_tau_m)*(1-f_tau_e+gamma_m)*u[2]*sigma_m)/((1-gamma_e)*sigma_e*f_u))
+    q_e = ((eta_m*(1-f_tau_m)*(1-f_tau_e+gamma_m)*u[2]*sigma_m)/((1-gamma_e)*sigma_e*f_u))
 
     #
     f_upsilon = gamma/(1-f_u)-gamma
 
     #
-    i = ((u[1]/(u[1]+eta_c))*f_upsilon+(eta_c/(u[1]+eta_c))*tau_m*(f_m*(u[3]+gamma_m*u[1]))-1)
+    i = ((u[1]/(u[1]+eta_m))*f_upsilon+(eta_m/(u[1]+eta_m))*tau_m*(f_m*(u[3]+gamma_m*u[1]))-1)
 
     #
     f_phi = phi/((1-u[5])^2)-phi
@@ -107,7 +107,7 @@ sigma_m   = 0.33
 sigma_e   = 0.33
 tau_m     = 4.0
 tau_e     = 4.0
-eta_c     = 1.0
+eta_m     = 1.0
 r_b       = 0.03
 phi       = 1.0
 gamma     = 0.7
@@ -120,7 +120,7 @@ uetoile   = 0.75
 #
 init=[rho0; kappa0; omega0; debt0; lambda0]
 parameters = [alpha; beta; delta_m; delta_e; f_m; f_e; gamma_m; gamma_e;
-              sigma_m; sigma_e; tau_m; tau_e; eta_c; r_b; phi; gamma;
+              sigma_m; sigma_e; tau_m; tau_e; eta_m; r_b; phi; gamma;
               kappa_0; kappa_1; kappa_2; kappa_3; uetoile]
 
 #
